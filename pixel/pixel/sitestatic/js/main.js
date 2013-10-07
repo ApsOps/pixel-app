7e34f869732743a670944fbf83005d5e8cc740b5
aps-sids <aps.sids@gmail.com>
2013-10-07 22:05:52 +0530

    Added slider

--- pixel/pixel/sitestatic/js/main.js
+++ pixel/pixel/sitestatic/js/main.js
@@ -2,3 +2,20 @@ $("nav>li>a").click(function(){
 	$("*").removeClass("active");
     $(this).addClass("active");
 });
+
+
+$(function() {
+    var select = $( "#minbeds" );
+    var slider = $( "<div id='slider'></div>" ).insertAfter( select ).slider({
+      min: 1,
+      max: 15,
+      range: "min",
+      value: select[ 0 ].selectedIndex + 1,
+      slide: function( event, ui ) {
+        select[ 0 ].selectedIndex = ui.value - 1;
+      }
+    });
+    $( "#minbeds" ).change(function() {
+      slider.slider( "value", this.selectedIndex + 1 );
+    });
+  });
