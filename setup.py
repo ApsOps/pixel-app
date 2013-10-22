14c986c0ad516b82d92a942bd3a7702bca3232f9
Amanpreet Singh <aps.sids@gmail.com>
2013-10-22 06:10:31 +0530

    Added pixel project files

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='Pixel',
       author='Amanpreet Singh',
       author_email='aps.sids@gmail.com',
       url='http://aps-sids.github.io',
-      install_requires=['Django>=1.5.4'],
+      install_requires=['Django>=1.5.4','django-facebook==5.2.9','Pillow==2.2.1'],
      )
